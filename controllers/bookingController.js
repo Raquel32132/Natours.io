797a6939b624084c9a1367578259ce70ffe31dde
henriqrbackes@gmail.com <raquel32132@hotmail.com>
2024-10-14 13:32:47 -0300

    still fixing

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -7,53 +7,51 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  // 1 - Get the currently booked tour
   const tour = await Tour.findById(req.params.tourId);
+
+  // 2 - Create checkout session
+  const product = await stripe.products.create({
+    name: `${tour.name} Tour`,
+    description: tour.summary,
+    images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
+  });
  
-  if (!tour) return next(new AppError('Tour not found!', 404));
+  const price = await stripe.prices.create({
+    product: product.id,
+    unit_amount: tour.price * 100,
+    currency: 'usd',
+  });
  
-  // create a checkout session
   const session = await stripe.checkout.sessions.create({
-    mode: 'payment',
     payment_method_types: ['card'],
     success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
-    client_reference_id: req.params.tourId,
+    client_reference_id: req.params.tourID,
+    mode: 'payment',
     line_items: [
       {
-        description: `${tour.summary}`,
-        price_data: {
-          unit_amount: tour.price * 100,
-          currency: 'usd',
-          product_data: {
-            name: tour.name,
-            description: `${tour.summary}`,
-            images: [
-              `${req.protocol}://${req.get('host')}/img/tours/${
-                tour.imageCover
-              }`
-            ]
-          }
-        },
-        quantity: 1
-      }
-    ]
+        price: price.id,
+        quantity: 1,
+      },
+    ],
   });
- 
-  // res.redirect(303, session.url); // Ignore this, only for front-end implementation via form action
+
+  // 3 - Create session as response
   res.status(200).json({
     status: 'success',
     session
-  });
+  })
 });
- 
+
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email }))._id;
   const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
- 
+
 exports.webhookCheckout = (req, res, next) => {
   const sig = req.headers['stripe-signature'];
   let event;

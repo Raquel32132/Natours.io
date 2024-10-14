fca6816406c8f6ceab45884aa3d43e9ef10a268d
henriqrbackes@gmail.com <raquel32132@hotmail.com>
2024-10-14 13:53:30 -0300

    teste

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -47,27 +47,27 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email }))._id;
-  const price = session.amount_total / 100;
+  const user = (await User.findOne({ email: session.customer_email })).id;
+  const price = session.display_items[0].amount / 100;
+
   await Booking.create({ tour, user, price });
-};
+}
 
 exports.webhookCheckout = (req, res, next) => {
-  const sig = req.headers['stripe-signature'];
+  const signature = req.headers['stripe-signature'];
+
   let event;
+
   try {
-    event = stripe.webhooks.constructEvent(
-      req.body,
-      sig,
-      process.env.WEBHOOK_SECRET
-    );
+    const event = stripe.webhooks.constructEvent(req.body, signature, process.env.STRIPE_WEBHOOK_SECRET);
+
   } catch (err) {
-    return res.status(400).send(`Webhook Error ${err.message}`);
+      return res.status(400).send(`Webhook error: ${err.message}`);
   }
- 
+
   if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
- 
+
   res.status(200).json({ received: true });
 };
 

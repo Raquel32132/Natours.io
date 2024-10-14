e3a22967f4d41dacf5afcf215f1e6ace1385eb23
henriqrbackes@gmail.com <raquel32132@hotmail.com>
2024-10-14 12:53:08 -0300

    fix commit

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -25,7 +25,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
  
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourID,
@@ -45,33 +45,32 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   })
 });
 
-const createBookingCheckout = async session => {
+const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email }))._id;
   const price = session.amount_total / 100;
-
-  console.log(tour, user, price)
-
+ 
   await Booking.create({ tour, user, price });
-}
-
-exports.webhookCheckout = (req, res, next) => {
-  const signature = req.headers['stripe-signature'];
-
-  let event;
-
-  try {
-    const event = stripe.webhooks.constructEvent(req.body, signature, process.env.STRIPE_WEBHOOK_SECRET);
-
-  } catch (err) {
-      return res.status(400).send(`Webhook error: ${err.message}`);
-  }
-
-  if (event.type === 'checkout.session.completed')
-    createBookingCheckout(event.data.object);
-
-  res.status(200).json({ received: true });
 };
+
+exports.webhooksCheckout = catchAsync(async (req, res, next) => {
+  const signature = req.headers['stripe-signature'];
+  let event;
+  try {
+    event = stripe.webhooks.constructEvent(
+      req.body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (err) {
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+ 
+  if (event.type === 'checkout.session.completed')
+    await createBookingCheckout(event.data.object);
+ 
+  res.status(200).json({ received: true });
+});
 
 exports.createBooking = createOne(Booking);
 

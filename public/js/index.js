4ff99ca56af65d50d50a5162fdb687cbdfc68045
henriqrbackes@gmail.com <raquel32132@hotmail.com>
2024-10-14 14:35:35 -0300

    Implemented stripe messages

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ import { login, logout } from './login';
 import { displayMap } from './mapbox';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
 
 // DOM elements
 const mapBox = document.getElementById('map');
@@ -60,3 +61,6 @@ if (bookBtn) {
     bookTour(tourId);
   })
 }
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alert) showAlert('success', alertMessage, 20);

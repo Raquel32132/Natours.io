bcb5dba6da60a85f84d160554e1be5af5a1100c7
henriqrbackes@gmail.com <raquel32132@hotmail.com>
2024-10-14 11:49:15 -0300

    Fixing price for stripe implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -25,7 +25,6 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
  
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    // success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
     success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
@@ -46,20 +45,10 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   })
 });
 
-// exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-//   // This is only temporary, unsecure
-//   const { tour, user, price } = req.query;
-
-//   if (!tour && !user && !price) return next();
-//   await Booking.create({ tour, user, price });
-
-//   res.redirect(req.originalUrl.split('?')[0]);
-// });
-
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.display_items[0].amount / 100;
+  const price = session.amount_total / 100;
 
   await Booking.create({ tour, user, price });
 }
